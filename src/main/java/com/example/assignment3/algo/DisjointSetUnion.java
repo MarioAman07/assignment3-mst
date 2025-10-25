78a8010de05672b72b3fe852f5bc727db90a7eaa
Aman Baku <bakuaman0770@gmail.com>
2025-10-25 07:36:16 +0500

    feat(ds): implement Disjoint Set Union (DSU) structure

---
+++ src/main/java/com/example/assignment3/algo/DisjointSetUnion.java
@@ -0,0 +1,70 @@
+package com.example.assignment3.algo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DisjointSetUnion {
+    private final Map<String, String> parent;
+    private final Map<String, Integer> size;
+    private long unionOperationCount;
+
+    public DisjointSetUnion(Iterable<String> elements) {
+        this.parent = new HashMap<>();
+        this.size = new HashMap<>();
+        this.unionOperationCount = 0;
+
+        for (String element : elements) {
+            parent.put(element, element);
+            size.put(element, 1);
+        }
+    }
+
+    public String find(String i) {
+        String root = parent.get(i);
+
+        if (root.equals(i)) {
+            return i;
+        }
+
+        root = find(root);
+        parent.put(i, root);
+
+        unionOperationCount++;
+
+        return root;
+    }
+
+
+    public boolean union(String i, String j) {
+        String rootI = find(i);
+        String rootJ = find(j);
+
+
+        if (rootI.equals(rootJ)) {
+            return false;
+        }
+
+        int sizeI = size.get(rootI);
+        int sizeJ = size.get(rootJ);
+
+        if (sizeI < sizeJ) {
+            parent.put(rootI, rootJ);
+            size.put(rootJ, sizeI + sizeJ);
+        } else {
+            parent.put(rootJ, rootI);
+            size.put(rootI, sizeI + sizeJ);
+        }
+
+        unionOperationCount++;
+
+        return true;
+    }
+
+    public long getUnionOperationCount() {
+        return unionOperationCount;
+    }
+
+    public void resetOperationCount() {
+        this.unionOperationCount = 0;
+    }
+}
